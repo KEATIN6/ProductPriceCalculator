f210c9c23945fdfd4e25cee48585fff12906782a
KEATIN6 <88174096+KEATIN6@users.noreply.github.com>
2022-04-17 11:56:42 -0400

    More updates to GUI

--- ProductRateCalculator.py
+++ ProductRateCalculator.py
@@ -19,17 +19,29 @@ FINANCE_RATE = .125
 
 # %%
 
-class ProductRateCalculator():
-    
-    def __init__(self, present_value, product_name=None):
-        self.present_value = int(present_value)
+class ProductOlv:
+    def __init__(self, name, retail, contract, mdaas, 
+                 finance_24m, finance_36m):
+        self.name = name
+        self.retail = retail
+        self.contract = contract
+        self.mdaas = mdaas
+        self.finance_24m = finance_24m
+        self.finance_36m = finance_36m
+        
+class ProductRateCalculator:
+    def __init__(self, present_value, product_name=""):
+        self.product_olv = None
+        self.present_value = float(present_value)
         rates = self.calculate_all_rates(self.present_value)
         self.rate_df = self.prepare_df_rates(rates)
-        if product_name :
-            self.product_name = product_name
+        self.product_name = product_name
+        if product_name != "":
             self.rename_results(product_name)     
         else:
             self.product_name = "Undefined Product"
+        self.store_rates(rates)
+        print("AHHHHH")
         
     def calculate_maas_rate(self, present_value):
         term = 24
@@ -51,16 +63,21 @@ class ProductRateCalculator():
         finance_36M_rate = self.calculate_finance_rate(present_value, 36)
         rates = {"Retail": present_value,
                  "Contract": contract_rate,
-                 "MaaS": maas_rate,
+                 "MDaaS": maas_rate,
                  "24MFinance": finance_24M_rate,
                  "36MFinance": finance_36M_rate}
         return rates
+    
+    def store_rates(self, rates):
+        self.product_olv = ProductOlv(
+            self.product_name, rates['Retail'], rates['Contract'], 
+            rates['MDaaS'], rates['24MFinance'], rates['36MFinance'])
 
     def prepare_df_rates(self, rates):
         df = pd.DataFrame()
         df['Retail'] = [rates['Retail']]
         df['Contract'] = [rates['Contract']]
-        df['MaaS'] = [rates['MaaS']]
+        df['MDaaS'] = [rates['MDaaS']]
         df['24MFinance'] = [rates['24MFinance']]
         df['36MFinance'] = [rates['36MFinance']]
         return df
@@ -107,6 +124,7 @@ class ProductRateCalculator():
 class ProductRates:
     def __init__(self, product_rate_list=None):
         
+        self.product_olv = []
         self.product_rate_df = pd.DataFrame()
         
         if product_rate_list and type(product_rate_list) == list:
@@ -117,7 +135,7 @@ class ProductRates:
         self.product_rate_df = pd.concat([self.product_rate_df, df_to_load])
             
     def add_product(self, product_price_tuple):
-        retail_rate, product_name= product_price_tuple
+        retail_rate, product_name = product_price_tuple
         prc = ProductRateCalculator(retail_rate, product_name)
         self.product_rate_df = pd.concat([self.product_rate_df, prc.rate_df])
         
@@ -148,6 +166,7 @@ class MainPanel(wx.Panel):
         self.btn01 = wx.Button(self, label="Add Record", size=(100,-1))
         self.btn01.Bind(wx.EVT_BUTTON, self.add_record)
         self.btn02 = wx.Button(self, label="Remove Record", size=(100,-1))
+        self.btn02.Bind(wx.EVT_BUTTON, self.delete_record)
         self.btn03 = wx.Button(self, label="Export Excel", size=(100,-1))
         
         button_sizer.Add(self.btn01, 0, wx.CENTER, 5)
@@ -159,15 +178,28 @@ class MainPanel(wx.Panel):
         self.SetSizerAndFit(main_vert_sizer)
         self.update_olv()
         
+    def store_product_rates(self, ProductOlv):
+        self.rate_results.append(ProductOlv)
+        self.update_olv()
         
     def add_record(self, event):
-        with RecordDialog() as dlg:
+        with RecordDialog(parent=self) as dlg:
             dlg.ShowModal()
+            
+    def delete_record(self, event):
+        selected_row = self.price_olv.GetSelectedObject()
+        if selected_row is None:
+            show_message("Please make sure to select a record!", "Error")
+            return
+        for result in self.rate_results:
+            if result == selected_row:
+                self.rate_results.remove(result)
+        self.update_olv()
     
         
     def update_olv(self):
         self.price_olv.SetColumns([
-            ColumnDefn('Product Name', 'left', 225, 'product_name'),
+            ColumnDefn('Product Name', 'left', 225, 'name'),
             ColumnDefn('Retail Rate', 'right', 100, 'retail'),
             ColumnDefn('Contract Rate', 'right', 100, 'contract'),
             ColumnDefn('MDaaS Rate', 'right', 100, 'mdaas'),
@@ -187,6 +219,12 @@ class MainFrame(wx.Frame):
         self.sizer.Add(self.panel_01, 1, wx.EXPAND)
         self.SetSizer(self.sizer)
         
+def show_message(message, caption, flag=wx.ICON_ERROR):
+    msg = wx.MessageDialog(None, message=message, caption=caption, style=flag)
+    msg.ShowModal()
+    msg.Destroy()
+        
+        
 # %%
 
 class RecordDialog(wx.Dialog):
@@ -204,18 +242,37 @@ class RecordDialog(wx.Dialog):
         font = wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD)
         
         product_name_lbl = wx.StaticText(self, label="Product Name", size=size)
-        product_name_ctrl = wx.TextCtrl(self, value="", size=(200,-1))
+        self.product_name_ctrl = wx.TextCtrl(self, value="", size=(200,-1))
         
-        main_sizer.Add(self.row_builder([product_name_lbl, product_name_ctrl]))
+        main_sizer.Add(self.row_builder([product_name_lbl, self.product_name_ctrl]))
         
-        product_rate_lbl = wx.StaticText(self, label="Retail", size=size)
-        product_rate_ctrl = wx.TextCtrl(self, value="", size=(200,-1))
+        product_rate_lbl = wx.StaticText(self, label="Retail Price", size=size)
+        self.product_rate_ctrl = wx.TextCtrl(self, value="", size=(200,-1))
         
-        main_sizer.Add(self.row_builder([product_rate_lbl, product_rate_ctrl]))
+        main_sizer.Add(self.row_builder([product_rate_lbl, self.product_rate_ctrl]))
+        
+        self.submit_btn = wx.Button(self, label="Submit")
+        self.submit_btn.Bind(wx.EVT_BUTTON, self.on_submit)
+        
+        main_sizer.Add(self.submit_btn, 0, wx.CENTER, 5)
         
         self.SetSizerAndFit(main_sizer)
         
-
+    def get_data(self):
+        data = {}
+        name = self.product_name_ctrl.GetValue()
+        rate = self.product_rate_ctrl.GetValue()
+        data['product_name'] = name
+        data['retail'] = float(rate)
+        return data
+    
+    def on_submit(self, event):
+        data = self.get_data()
+        prc = ProductRateCalculator(data['retail'], data['product_name'])
+        self.parent.store_product_rates(prc.product_olv)
+        self.Close()
+        
+    
     def row_builder(self, widgets):
         sizer = wx.BoxSizer(wx.HORIZONTAL)
         label, text = widgets
